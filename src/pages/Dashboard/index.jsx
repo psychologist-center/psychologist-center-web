590af698b537d07db815defeaabc02b5dbe9213e
arthursvpb <arthurandroid15@gmail.com>
2022-09-18 21:08:07 -0300

    add requests to backend

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,4 +1,8 @@
+import { useCallback, useEffect, useState } from 'react'
+
 import { Stat, StatLabel, StatNumber, HStack, Box } from '@chakra-ui/react'
+
+import { api } from '../../services/api'
 
 function Card({ title, number, ...rest }) {
   return (
@@ -29,15 +33,34 @@ function Card({ title, number, ...rest }) {
 }
 
 export function DashboardPage() {
+  const [data, setData] = useState({})
+
+  const fetchData = useCallback(() => {
+    const URLs = ['user/list/patient', 'session/list']
+    const URLsToFetch = URLs.map((url) => api.get(url))
+
+    Promise.all(URLsToFetch).then((resolved) => {
+      setData({
+        ...data,
+        patients: resolved[0].data.total,
+        sessions: resolved[1].data.total,
+      })
+    })
+  }, [data])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
   return (
     <HStack flexWrap="wrap" gap={6}>
-      <Card title="Sessões agendadas (dia)" number="0" />
-      <Card title="Sessões agendadas (mês)" number="0" />
+      <Card title="Sessões agendadas (dia)" number={data.sessions} />
+      <Card title="Sessões agendadas (mês)" number={data.sessions} />
       <Card title="Sessões canceladas (mês)" number="0" />
-      <Card title="Total de pacientes cadastrados" number="0" />
-      <Card title="Total de sessões (individuais)" number="0" />
-      <Card title="Total de sessões (dupla)" number="0" />
-      <Card title="Total de sessões (grupo)" number="0" />
+      <Card title="Total de pacientes cadastrados" number={data.patients} />
+      <Card title="Total de sessões (individuais)" number={data.sessions} />
+      <Card title="Total de sessões (dupla)" number={data.sessions} />
+      <Card title="Total de sessões (grupo)" number={data.sessions} />
     </HStack>
   )
 }
